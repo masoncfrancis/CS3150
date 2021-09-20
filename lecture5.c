441fa9298cf5003cf2c0d6ebf6a0221194539ef3
masoncfrancis <masoncfrancis@gmail.com>
2021-09-20 16:43:23 -0600

    Add code to print 1 through 10 except 5 using a continue statement.

--- lecture5.c
+++ lecture5.c
@@ -6,10 +6,24 @@
 
 int main(void){
 
+    printf("Demonstrating do-while loop\n");
+
     int num = 1;
     // Do while loop to print 1 through 10
 
     do {
+        printf("%d\n", num++);
+    } while (num <= 10);
+
+    printf("\nDemonstrating continue statement\n");
+
+    // Demonstrate continue statement
+    num = 1;
+    do {
+        if(num == 5){
+            num++;
+            continue;
+        }
         printf("%d\n", num++);
     } while (num <= 10);
 
